22cd562aec67b0f99331077c881e311dbddb0724
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-07-09 23:39:19 -0500

    Ejercicio3 - Actualización: Mejoras visuales.

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -3,21 +3,27 @@ using namespace std;
 
 int main(){
     int n,A[30],aux;
-    cout<<"ingrese el numero de elementos: ";
+    cout<<"*******************************************************"<<endl;
+	cout<<"*****        SERIE DE ELEMENTOS INVERTIDOS        *****"<<endl;
+	cout<<"*******************************************************"<<endl<<endl;
+    cout<<"Ingrese el numero de elementos:  ";
     cin>>n;
+    cout<<endl;
     for(int i=0;i<=n-1;i++){
-        cout<<"Ingrese el valor del elemento: ";
+        cout<<"Ingrese el valor numerico del elemento:  ";
         cin>>A[i];
     }
+    cout<<endl<<endl;
     for(int i=0;i<n/2;i++){
         aux=A[i];
         A[i]=A[n-i-1];
         A[n-i-1]=aux;
     }
-    for(int i=0;i<=n-1;i++){
-        cout<<"Pocision "<<i<<" - ";
-        cout<<A[i]<<endl;
-
+    cout<<"******************************************************"<<endl;
+	cout<<"******           LISTA DE POSCICIONES           ******"<<endl;
+    cout<<"******************************************************"<<endl<<endl;
+	for(int i=0;i<=n-1;i++){
+        cout<<"Pocision #"<<(i+1)<<"   -   "<<A[i]<<endl;
     }
     return 0;
 }
