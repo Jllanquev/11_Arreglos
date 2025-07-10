7b03d43081ca3b9f25650bb32b4ce2ff4f592407
EverVargash <ebvargash@unjbg.edu.pe>
2025-07-09 22:51:30 -0500

    Modificando mensajes y nuevas funionalidadades

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -2,21 +2,26 @@
 using namespace std;
 
 int main(){
+	string op;
 	int elementos,mayor,A[10];
-	cout<<"ingrese la cantidad de elementos que desea ingresar: ";
-	cin>>elementos;
-	for(int i=0;i<=elementos-1;i++){
-		cin>>A[i];
-	}
-	mayor=A[0];
-	
-	for(int i=1;i<=elementos-1;i++){
-		if(A[i]>mayor){
-			mayor=A[i];
+	do{
+		cout<<"ingrese la cantidad de elementos(Max.10): "<<endl;
+		cin>>elementos;
+		if(elementos>10) cout<<"El maximo de lementos sera solo 10"<<endl;
+		else {
+			for(int i=0;i<elementos;i++){
+				cout<<"El elemento "<<i+1<<" sera: "<<endl;
+				cin>>A[i];
+			}
+		mayor=A[0];
+			for(int i=1;i<=elementos-1;i++){
+				if(A[i]>mayor){
+					mayor=A[i];
+				}
+			}
+			cout<<"el mayor numero es: "<<mayor<<endl;
 		}
-	}
-	cout<<"el mayor numero es: "<<mayor;
-	
-	
+		cout<<"Presione y para volver a intentar"<<endl; cin>>op;
+	} while(op=="y");
 	return 0;
 }
